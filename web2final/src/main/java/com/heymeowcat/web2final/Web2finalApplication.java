1fb7e91ca56165ff0e447f07ec663683b9b9a64c
Vidura Fernando <heyvidura@gmail.com>
2021-04-06 14:47:01 +0530

    Initialized Project 😀

---
+++ web2final/src/main/java/com/heymeowcat/web2final/Web2finalApplication.java
@@ -0,0 +1,13 @@
+package com.heymeowcat.web2final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2finalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2finalApplication.class, args);
+	}
+
+}
