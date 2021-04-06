2ec9506382d96bb56f01c578132e2840e4dc795f
dulminkannangara <79748782+dulminkannangara@users.noreply.github.com>
2021-04-06 16:01:24 +0530

    Feed-controller,repo,service
    Create Feed-controller,repo,service

---
+++ web2final/src/main/java/com/heymeowcat/web2final/controller/FeedController.java
@@ -0,0 +1,14 @@
+package com.heymeowcat.web2final.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feed")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
+public class FeedController {
+
+	
+	
+}
---
+++ web2final/src/main/java/com/heymeowcat/web2final/service/FeedService.java
@@ -0,0 +1,18 @@
+package com.heymeowcat.web2final.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heymeowcat.web2final.repository.FeedRepository;
+
+@Service
+public class FeedService {
+
+	@Autowired
+	FeedRepository repository;
+	
+	
+	
+	
+	
+}
