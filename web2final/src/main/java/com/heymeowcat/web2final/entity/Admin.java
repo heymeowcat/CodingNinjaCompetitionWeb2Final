618279dfd28a6168d48e6eb96b30425bfcca5043
Vidura Fernando <heyvidura@gmail.com>
2021-04-06 17:50:55 +0530

    Default SerialID added

--- web2final/src/main/java/com/heymeowcat/web2final/entity/Admin.java
+++ web2final/src/main/java/com/heymeowcat/web2final/entity/Admin.java
@@ -19,6 +19,10 @@ import lombok.NoArgsConstructor;
 @Table(name="admin")
 public class Admin implements Serializable{
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
