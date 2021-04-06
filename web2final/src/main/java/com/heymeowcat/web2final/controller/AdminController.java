7036a7de29deec88527c44196db213cf8395e4a7
Vidura Fernando <heyvidura@gmail.com>
2021-04-06 21:23:18 +0530

    Better Security 😀

--- web2final/src/main/java/com/heymeowcat/web2final/controller/AdminController.java
+++ web2final/src/main/java/com/heymeowcat/web2final/controller/AdminController.java
@@ -3,6 +3,12 @@ package com.heymeowcat.web2final.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,37 +19,66 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.heymeowcat.web2final.Config.JwtTokenUtil;
 import com.heymeowcat.web2final.entity.Admin;
+import com.heymeowcat.web2final.model.JwtRequest;
+import com.heymeowcat.web2final.model.JwtResponse;
 import com.heymeowcat.web2final.service.AdminService;
 
 @RestController
 @RequestMapping("/admin")
-@CrossOrigin(origins = "*",allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AdminController {
 
 	@Autowired
 	AdminService service;
-	
-	@GetMapping("/getAllAdmins")
-	public List<Admin> getAllAdmins(){
-	  return service.getAllAdmin();
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = service.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
 	}
-	
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+
+	@GetMapping("/getAllAdmins")
+	public List<Admin> getAllAdmins() {
+		return service.getAllAdmin();
+	}
+
 	@PostMapping("/addAdmin")
-	public Admin saveAdmin(@RequestBody Admin admin){
+	public Admin saveAdmin(@RequestBody Admin admin) {
 		return service.addAdmin(admin);
 	}
-	
+
 	@PutMapping("/updateAdmin")
 	public Admin updateAdmin(@RequestBody Admin admin) {
 		return service.updateAdmin(admin);
 	}
-	
+
 	@DeleteMapping("/deleteAdmin/{id}")
 	public String deleteAdmin(@PathVariable int id) {
 		return service.deleteAdmin(id);
 	}
-	
-	
-	
+
 }
